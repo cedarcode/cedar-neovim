29d7463dcf9fa39da80401fde2cae37ff1cb78b2
Nicolás Buero <nicolasbuero9@gmail.com>
2025-03-21 22:58:02 -0300

    add lsp config

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -11,3 +11,12 @@ vim.opt.tabstop = 2                   -- Number of spaces a tab counts for
 vim.opt.undofile = true               -- Enable persistent undo
 
 vim.wo.number = true
+
+-- LSP keymaps
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", {})
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, {})
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
