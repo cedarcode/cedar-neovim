f5ef2f5cd68233d09a7ba94098286df340b2d28b
Nicolás Buero <nicolasbuero9@gmail.com>
2025-03-27 12:31:02 -0300

    keymaps to navigate between panes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,9 @@ vim.keymap.set("n", "<C-b>", function() Snacks.picker.buffers() end)
 vim.keymap.set("n", "<C-/>", function() Snacks.picker.grep() end)
 vim.keymap.set("n", "<C-n>", function() Snacks.explorer() end)
 vim.keymap.set("n", "<C-p>", function() Snacks.picker.files() end)
+
+-- Navigation keymaps
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
