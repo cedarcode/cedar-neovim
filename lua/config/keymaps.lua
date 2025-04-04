73f66ee5ca1c1fd3d4d574fe197337dcc5c58c18
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-04-04 11:31:54 -0300

    chore(snacks): use `ripgrep` for file searching
    See `Sancks` docs:
    https://github.com/folke/snacks.nvim/blob/bc0630e43be5699bb94dadc302c0d21615421d93/docs/picker.md?plain=1#L1042

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 -- Snacks keymaps
 local opts = { hidden = true }
 
-vim.keymap.set("n", "<C-b>", function() Snacks.picker.buffers(opts) end)
+vim.keymap.set("n", "<C-b>", function() Snacks.picker.buffers({ hidden = true, cmd = "rg" }) end)
 vim.keymap.set("n", "<C-/>", function() Snacks.picker.grep(opts) end)
 vim.keymap.set("n", "<C-n>", function() Snacks.explorer(opts) end)
-vim.keymap.set("n", "<C-p>", function() Snacks.picker.files(opts) end)
+vim.keymap.set("n", "<C-p>", function() Snacks.picker.files({ hidden = true, cmd = "rg" }) end)
