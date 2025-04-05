7403a98cc158cfeb11ac502520985a2c6c62458a
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-04-05 19:58:42 -0300

    refactor: fix autocommand cwd handling and add caching
    - Fixed autocommand that sets the cwd to the root of the project/gem, ensuring it works properly when entering a gem.
    - Introduced caching for better performance.
    - Refactor based on guidance from https://www.reddit.com/r/neovim/comments/zy5s0l/you_dont_need_vimrooter_usually_or_how_to_set_up

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,13 +1,31 @@
 local autocmd = vim.api.nvim_create_autocmd
 
--- Change the working directory only if the file is not in a subdirectory of the current one
-autocmd("BufWinEnter", {
+local root_names = { '.git', 'LICENSE', 'LICENSE.txt', 'MIT-LICENSE' }
+
+local root_cache = {}
+
+local function find_root(path)
+  local root_file = vim.fs.find(root_names, { path = path, upward = true })[1]
+  if not root_file then return nil end
+
+  local root = vim.fs.dirname(root_file)
+  root_cache[path] = root
+
+  return root
+end
+
+autocmd({ "BufWinEnter", "BufWinLeave" }, {
   pattern = "*",
   callback = function()
-    local file_dir = vim.fn.expand('%:p:h')
-    local cwd = vim.fn.getcwd()
-    if not string.match(file_dir, "^" .. cwd) then
-      vim.cmd('silent! lcd ' .. file_dir)
+    -- Skip snacks windows
+    if vim.bo.filetype:match("^snacks") then return end
+
+    local path = vim.fn.expand('%:p:h')
+
+    local root = root_cache[path] or find_root(path)
+
+    if root then
+      vim.cmd('silent! cd ' .. root)
     end
   end,
 })
