c55dbec2e8711a89a0fa5779ff4506f3a5847329
Nicolás Buero <nicolasbuero9@gmail.com>
2025-04-10 20:36:09 -0300

    remove pending migration pop up

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,15 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
-      lspconfig.ruby_lsp.setup({})
+      lspconfig.ruby_lsp.setup({
+        init_options = {
+          addonSettings = {
+            ["Ruby LSP Rails"] = {
+              enablePendingMigrationsPrompt = false,
+            },
+          },
+        },
+      })
       lspconfig.ts_ls.setup({})
     end,
   },
