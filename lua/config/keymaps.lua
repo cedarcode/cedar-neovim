a30eb6a5554164912d454d784c2a88654f4d3b4c
Santiago <santiago.rodriguez@cedarcode.com>
2025-04-22 16:32:23 -0300

    feat: fix grep keymap not working in tmux

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,4 @@ vim.keymap.set("n", "<C-b>", function() Snacks.picker.buffers({ hidden = true, c
 vim.keymap.set("n", "<C-/>", function() Snacks.picker.grep(opts) end)
 vim.keymap.set("n", "<C-n>", function() Snacks.explorer(opts) end)
 vim.keymap.set("n", "<C-p>", function() Snacks.picker.files({ hidden = true, cmd = "rg" }) end)
+vim.keymap.set("n", "<C-_>", "<C-/>", { remap = true })
