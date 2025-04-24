0620f7d75415db4e5dab62aba65f1f8d0bad19b5
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-04-24 16:19:45 -0300

    Add completion plugin (#43)
    * feat: add `nvim-cmp` as the completion plugin
    
    * feat: add snippets as a completion source
    
    * feat: disable completion plugin by default

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,111 @@
+return {
+  "hrsh7th/nvim-cmp",
+  version = false,
+  enabled = false,
+  event = { "InsertEnter" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+  },
+  opts = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+    local cmp = require("cmp")
+    local defaults = require("cmp.config.default")()
+    local auto_select = true
+    return {
+      completion = {
+        completeopt = "menu,menuone,noinsert"
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<CR>"] = cmp.mapping.confirm(),
+        ["<C-CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end, -- For adding a newline when the completion menu is open
+      }),
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "luasnip" },
+      }, {
+        { name = "buffer" },
+      }),
+      formatting = {
+        format = function(entry, item)
+          local icons = {
+            Array         = " ",
+            Boolean       = "󰨙 ",
+            Class         = " ",
+            Codeium       = "󰘦 ",
+            Color         = " ",
+            Control       = " ",
+            Collapsed     = " ",
+            Constant      = "󰏿 ",
+            Constructor   = " ",
+            Copilot       = " ",
+            Enum          = " ",
+            EnumMember    = " ",
+            Event         = " ",
+            Field         = " ",
+            File          = " ",
+            Folder        = " ",
+            Function      = "󰊕 ",
+            Interface     = " ",
+            Key           = " ",
+            Keyword       = " ",
+            Method        = "󰊕 ",
+            Module        = " ",
+            Namespace     = "󰦮 ",
+            Null          = " ",
+            Number        = "󰎠 ",
+            Object        = " ",
+            Operator      = " ",
+            Package       = " ",
+            Property      = " ",
+            Reference     = " ",
+            Snippet       = "󱄽 ",
+            String        = " ",
+            Struct        = "󰆼 ",
+            Supermaven    = " ",
+            TabNine       = "󰏚 ",
+            Text          = " ",
+            TypeParameter = " ",
+            Unit          = " ",
+            Value         = " ",
+            Variable      = "󰀫 ",
+          }
+          if icons[item.kind] then
+            item.kind = icons[item.kind] .. item.kind
+          end
+
+          local widths = {
+            abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+            menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+          }
+
+          for key, width in pairs(widths) do
+            if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+              item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+            end
+          end
+
+          return item
+        end,
+      },
+      sorting = defaults.sorting,
+    }
+  end,
+}
