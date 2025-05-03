ea128cdc49559c3a8930c1f78cdc827ccdb151a6
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-05-03 09:46:01 -0300

    chore: add useful keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,10 +3,22 @@ vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end)
 vim.keymap.set("n", "gr", function() Snacks.picker.lsp_references() end)
 
 -- Snacks keymaps
-local opts = { hidden = true }
+local opts = { hidden = true, ignored = true, cmd = "rg" }
 
-vim.keymap.set("n", "<C-b>", function() Snacks.picker.buffers({ hidden = true, cmd = "rg" }) end)
-vim.keymap.set("n", "<C-/>", function() Snacks.picker.grep(opts) end)
+-- Explorer
 vim.keymap.set("n", "<C-n>", function() Snacks.explorer(opts) end)
-vim.keymap.set("n", "<C-p>", function() Snacks.picker.files({ hidden = true, cmd = "rg" }) end)
+
+-- Pickers
+vim.keymap.set("n", "<C-p>", function() Snacks.picker.files(opts) end)
+vim.keymap.set("n", "<C-b>", function() Snacks.picker.buffers(opts) end)
+vim.keymap.set("n", "<C-g>", function() Snacks.picker.git_status(opts) end)
+vim.keymap.set("n", "<C-q>", function() Snacks.picker.qflist(opts) end)
+vim.keymap.set({"n", "v"}, "<C-s>", function() Snacks.picker.grep_word(opts) end)
+
+-- Grep
+vim.keymap.set("n", "<C-/>", function() Snacks.picker.grep(opts) end)
 vim.keymap.set("n", "<C-_>", "<C-/>", { remap = true })
+
+-- Git keymaps
+vim.keymap.set({"n", "v"}, "gl", ":Git blame<CR>")
+vim.keymap.set({"n", "v"}, "gb", ":GBrowse<CR>")
