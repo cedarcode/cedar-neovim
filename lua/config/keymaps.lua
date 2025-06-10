1a0aa30c2d0260faf380c2d0f467fc48fc33a050
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-06-10 20:02:27 -0300

    fix: not working files picker after opening grep picker
    This is probably because we're passing the cmd argument to the grep
    picker

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,7 @@ vim.keymap.set("n", "gr", function() Snacks.picker.lsp_references() end)
 
 -- Snacks keymaps
 local opts = { hidden = true, ignored = true, cmd = "rg", exclude= { ".git", "tmp", "node_modules", "log", "coverage" } }
+local opts2 = { hidden = true, ignored = true, exclude= { ".git", "tmp", "node_modules", "log", "coverage" } }
 
 -- Explorer
 vim.keymap.set("n", "<C-n>", function() Snacks.explorer(opts) end)
@@ -17,7 +18,7 @@ vim.keymap.set("n", "<C-q>", function() Snacks.picker.qflist(opts) end)
 vim.keymap.set({"n", "v"}, "<C-s>", function() Snacks.picker.grep_word(opts) end)
 
 -- Grep
-vim.keymap.set("n", "<C-/>", function() Snacks.picker.grep(opts) end)
+vim.keymap.set("n", "<C-/>", function() Snacks.picker.grep(opts2) end)
 vim.keymap.set("n", "<C-_>", "<C-/>", { remap = true })
 
 -- Git keymaps
