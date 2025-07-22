ce34bbf1381eca5c33e6febdda07b7ed866e06e0
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-07-22 16:04:00 -0300

    chore: remove default config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,6 @@ return {
 
     configs.setup({
       ensure_installed = { "c", "lua", "vim", "vimdoc", "ruby", "javascript", "html", "embedded_template", "typescript", "tsx" },
-      sync_install = false,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = { "ruby", "embedded_template" }
