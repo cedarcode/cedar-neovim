1381699cb59c4b072f092c5d51cdb5954097c7b1
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-07-23 11:57:15 -0300

    chore: disable notification for updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,7 +42,10 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    notify = false, -- disable notifications for updates
+  },
   -- ui config
     ui = {
       border = "rounded",
