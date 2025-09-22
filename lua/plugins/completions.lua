f64ecfdc1d4ec8458afd315ae6a3a97581679f26
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-09-22 11:31:04 -0300

    feat(completions): integrate copilot.lua

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -16,6 +16,12 @@ return {
     local cmp = require("cmp")
     local defaults = require("cmp.config.default")()
     local auto_select = true
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+    end
+
     return {
       completion = {
         completeopt = "menu,menuone,noinsert"
@@ -40,9 +46,10 @@ return {
         { name = "nvim_lsp" },
         { name = "path" },
         { name = "luasnip" },
+        { name = "copilot", group_index = 2},
       }, {
-        { name = "buffer" },
-      }),
+          { name = "buffer" },
+        }),
       formatting = {
         format = function(entry, item)
           local icons = {
@@ -105,13 +112,39 @@ return {
           return item
         end,
       },
-      sorting = defaults.sorting,
-    },
-    cmp.setup({
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          require("copilot_cmp.comparators").prioritize,
+
+          -- Below is the default comparitor list and order for nvim-cmp
+          cmp.config.compare.offset,
+          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
       },
-    })
+      cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = {
+          ["<CR>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
+            end
+          end),
+        },
+      })
+    }
   end,
 }
