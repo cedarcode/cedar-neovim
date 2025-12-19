b9c771f59213bca6adf66b14a797d57a1d587bac
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-12-19 12:02:35 -0300

    feat: additional configurations to `nvim-cmp`

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -2,7 +2,7 @@ return {
   "hrsh7th/nvim-cmp",
   version = false,
   enabled = false,
-  event = { "InsertEnter" },
+  event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
@@ -13,19 +13,25 @@ return {
   },
   opts = function()
     require("luasnip.loaders.from_vscode").lazy_load()
+
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
     local cmp = require("cmp")
     local defaults = require("cmp.config.default")()
     local auto_select = true
     return {
+      auto_brackets = {}, -- configure any filetype to auto add brackets
       completion = {
-        completeopt = "menu,menuone,noinsert"
+        completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
       },
+      preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
       mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<CR>"] = cmp.mapping.confirm(),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = auto_select }),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<C-CR>"] = function(fallback)
           cmp.abort()
           fallback()
@@ -104,6 +110,12 @@ return {
 
           return item
         end,
+      },
+      experimental = {
+        -- only show ghost text when we show ai completions
+        ghost_text = vim.g.ai_cmp and {
+          hl_group = "CmpGhostText",
+        } or false,
       },
       sorting = defaults.sorting,
     },
