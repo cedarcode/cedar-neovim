20528ea7994f79c93376e8a4643633ee84026265
Nicolas Temciuc <nicotemciuc@gmail.com>
2026-03-10 14:00:58 -0300

    chore: allow calling the file picker on oil's buffer (#75)

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,4 +4,14 @@ return {
   opts = {},
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   lazy = false,
+  config = function()
+    require("oil").setup {
+      keymaps = {
+        ["<C-p>"] = false,
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
+  end,
 }
