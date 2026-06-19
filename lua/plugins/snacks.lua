6dd0829c5fadb061eb0db1fd53c911f3fce18d50
Nicolas Temciuc <nicotemciuc@gmail.com>
2026-06-19 11:38:30 -0300

    animations for dashboard :)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,15 +1,29 @@
+local chosen_splash
+
 return {
   "folke/snacks.nvim",
   dependencies = {
     "echasnovski/mini.icons",
+    "amansingh-afk/milli.nvim",
   },
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
-  opts = {
+  opts = function()
+    local milli = require("milli")
+    local names = milli.list()
+    math.randomseed(os.time())
+    chosen_splash = names[math.random(#names)]
+    local splash = milli.load({ splash = chosen_splash })
+    return {
     bigfile = { enabled = true },
     bufdelete = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = table.concat(splash.frames[1], "\n"),
+      },
+    },
     explorer = { enabled = false },
     indent = { enabled = true },
     input = { enabled = true },
@@ -41,5 +55,10 @@ return {
     scope = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
-  },
+    }
+  end,
+  config = function(_, opts)
+    require("snacks").setup(opts)
+    require("milli").snacks({ splash = chosen_splash, loop = true })
+  end,
 }
